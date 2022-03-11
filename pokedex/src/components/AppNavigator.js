89be1a1a3b6d0636c377a4a348fc72a55fb660ca
Michael Stark <mikedstark@gmail.com>
2022-03-11 16:49:22 -0600

    end of 4th lecture

--- pokedex/src/components/AppNavigator.js
+++ pokedex/src/components/AppNavigator.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { Link } from 'react-router-dom'
+
+const useStyles = makeStyles((theme) => ({
+    AppBar: {
+        backgroungColor: 'black'
+    },
+    link: {
+      textDecoration: 'none'
+    },
+    title: {
+      cursor: 'pointer',
+      color: 'white'
+    }
+}))
 
 export default function AppNavigator() {
-  return (
-    <div>
+  const classes = useStyles()
 
-    </div>
+  return (
+    <AppBar className={classes.AppBar} position='fixed' variant='h6'>
+        <Toolbar>
+            <Link to="/" className={classes.link}>
+              <Typography className={classes.title}>Pokedex</Typography>
+            </Link>
+        </Toolbar>
+    </AppBar>
   )
 }
